9a678d21d9152f22dc75862788a904f12cd0f16e
uh-kay <konstantius.kevin@gmail.com>
2025-04-13 22:45:37 +0700

    Add user authentication

--- internal/data/users.go
+++ internal/data/users.go
@@ -15,6 +15,8 @@ var (
 	ErrDuplicateEmail = errors.New("duplicate email")
 )
 
+var AnonymousUser = &User{}
+
 type User struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
@@ -28,6 +30,10 @@ type User struct {
 type password struct {
 	plaintext *string
 	hash      []byte
+}
+
+func (u *User) IsAnonymous() bool {
+	return u == AnonymousUser
 }
 
 func (p *password) Set(plaintextPassword string) error {
